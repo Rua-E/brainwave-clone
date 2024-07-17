89d34f5431dc04af70e698cd0559e81c2f6978ac
Rua <rua.elkasheef@gmail.com>
2024-07-17 18:55:14 -0400

    adding section crosses

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section from './Section';
+
+const Collaboration = () => {
+  return (
+    <Section crosses >
+      collaboration
+    </Section>
+  )
+}
+
+export default Collaboration;
